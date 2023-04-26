ae61acc49a3f6238475cf0204c3baa96413b6da3
Alampi Kevin <Alampi.kevin@gmail.com>
2023-04-26 19:21:46 +0200

    ajout composant Layout

--- src/App.js
+++ src/App.js
@@ -1,36 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import PageDeTest from './pages/PageDeTest';
+import Layout from './components/Layout';
 
-
-import Nav from './components/Nav';
-import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <header>
-          <Nav />
-        </header>
-
-          <Routes>
-            {/* Pas encore d'attribut element={ nom de page } dans toutes les routes */}
-            <Route path='/' element={<Home/>} />
-            <Route path='/HowItWorks' element={<PageDeTest />} />
-            <Route path='/SearchRides' />
-            <Route path='/PostARide' />
-            <Route path='/Account' />
-          </Routes>
-
-        <footer>
-          <Footer/>
-        </footer>
-        
-      </BrowserRouter>
-    </>
+    <Layout>
+        <Routes>
+          {/* Pas encore d'attribut element={ nom de page } dans toutes les routes */}
+          <Route path='/' element={<Home/>} />
+          <Route path='/HowItWorks' element={<PageDeTest />} />
+          <Route path='/SearchRides' />
+          <Route path='/PostARide' />
+          <Route path='/Account' />
+        </Routes>
+    </Layout>
   );
 };
 
