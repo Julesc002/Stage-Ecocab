f0c46b239dc8814a4ca17f553e8b8c0819ddacad
Alampi Kevin <Alampi.kevin@gmail.com>
2023-04-26 16:46:33 +0200

    rectifs css et set up dossier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import Nav from './components/Nav';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
 import PageDeTest from './pages/PageDeTest';
+
+
+import Nav from './components/Nav';
 import Footer from './components/Footer';
 
 const App = () => {
@@ -11,16 +15,20 @@ const App = () => {
         <header>
           <Nav />
         </header>
+
+          <Routes>
+            {/* Pas encore d'attribut element={ nom de page } dans toutes les routes */}
+            <Route path='/' element={<Home/>} />
+            <Route path='/HowItWorks' element={<PageDeTest />} />
+            <Route path='/SearchRides' />
+            <Route path='/PostARide' />
+            <Route path='/Account' />
+          </Routes>
+
         <footer>
           <Footer/>
         </footer>
-        <Routes>
-          {/* Pas encore d'attribut element={ nom de page } dans toutes les routes */}
-          <Route path='/HowItWorks' element={<PageDeTest />} />
-          <Route path='/SearchRides' />
-          <Route path='/PostARide' />
-          <Route path='/Account' />
-        </Routes>
+        
       </BrowserRouter>
     </>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import '../styles/footer.css';
 
 const Footer = () => {
     return (
