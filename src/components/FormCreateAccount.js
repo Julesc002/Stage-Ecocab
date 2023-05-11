b075be94b2b39e8dc3be71c09df1df0d8b6d2474
Saskue <clem.rieuneau@gmail.com>
2023-05-11 11:32:40 +0200

    Back : Vérification des informations du formulaire de création de compte + ajout de la date de départ et d'arrivée dans le formulaire d'ajout de trajet

--- src/components/FormCreateAccount.js
+++ src/components/FormCreateAccount.js
@@ -1,37 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const FormCreateAccount = () => {
+
+    const [email, setEmail] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [birthDate, setBirthDate] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [generalConditions, setGeneralConditions] = useState(false);
+
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setErrorMessage("");
+        let missingFields = "";
+        if (!email) {
+            missingFields += "Email, ";
+        }
+        if (!lastName) {
+            missingFields += "Nom, ";
+        }
+        if (!firstName) {
+            missingFields += "Prénom, ";
+        }
+        if (!password) {
+            missingFields += "Mot de passe, ";
+        }
+        if (!confirmPassword) {
+            missingFields += "Confirmer mot de passe, ";
+        }
+        if (!birthDate) {
+            missingFields += "Date de naissance, ";
+        }
+        if (!phoneNumber) {
+            missingFields += "Numéro de téléphone, ";
+        }
+        if (missingFields) {
+            missingFields = missingFields.slice(0, -2); // Supprime la virgule et l'espace à la fin de la chaîne
+            setErrorMessage(`Les champs suivants sont manquants: ${missingFields}`);
+        } else if (password !== confirmPassword) {
+            setErrorMessage("Les mots de passe ne correspondent pas");
+        } else if (!generalConditions) {
+            setErrorMessage("Veuillez valider les conditions générales d'utilisation");
+        } else {
+            // Faire les traitements
+        }
+    }
+
     return (
         <div className='createAccountFormContainer'>
             <h1 className='createAccountFormContainer_mainTitleForm'> Inscription </h1>
-            <form className='loginFormContainer_loginForm'>
+            <form className='loginFormContainer_loginForm' onSubmit={handleSubmit}>
                 <label htmlFor='email' className='createAccountFormContainer_loginForm_label'> Email </label>
-                <input type="text" id='email' className='createAccountFormContainer_loginForm_inputText' />
+                <input type="text" id='email' value={email} className='createAccountFormContainer_loginForm_inputText' onChange={(e) => setEmail(e.target.value)} />
 
                 <label htmlFor='lastName' className='createAccountFormContainer_loginForm_label'> Nom </label>
-                <input type="text" id='lastName' className='createAccountFormContainer_loginForm_inputText' />
+                <input type="text" id='lastName' value={lastName} className='createAccountFormContainer_loginForm_inputText' onChange={(e) => setLastName(e.target.value)} />
 
                 <label htmlFor='firstName' className='createAccountFormContainer_loginForm_label'> Prénom </label>
-                <input type="text" id='firstName' className='createAccountFormContainer_loginForm_inputText' />
+                <input type="text" id='firstName' value={firstName} className='createAccountFormContainer_loginForm_inputText' onChange={(e) => setFirstName(e.target.value)} />
 
                 <label htmlFor='password' className='createAccountFormContainer_loginForm_label'> Mot de passe </label>
-                <input type="password" id='password' className='createAccountFormContainer_loginForm_inputPassword' />
+                <input type="password" id='password' value={password} className='createAccountFormContainer_loginForm_inputPassword' onChange={(e) => setPassword(e.target.value)} />
 
                 <label htmlFor='confirmPassword' className='createAccountFormContainer_loginForm_label'> Confirmer Mot de passe </label>
-                <input type="password" id='confirmPassword' className='createAccountFormContainer_loginForm_inputPassword' />
+                <input type="password" id='confirmPassword' value={confirmPassword} className='createAccountFormContainer_loginForm_inputPassword' onChange={(e) => setConfirmPassword(e.target.value)} />
 
                 <label htmlFor='birthDate' className='createAccountFormContainer_loginForm_label'> Date de naissance </label>
-                <input type="date" id='birthDate' className='createAccountFormContainer_loginForm_inputDate' />
+                <input type="date" id='birthDate' value={birthDate} className='createAccountFormContainer_loginForm_inputDate' onChange={(e) => setBirthDate(e.target.value)} />
 
                 <label htmlFor='phoneNumber' className='createAccountFormContainer_loginForm_label'> Téléphone </label>
-                <input type="tel" id='phoneNumber' className='createAccountFormContainer_loginForm_inputTel' />
+                <input type="tel" id='phoneNumber' value={phoneNumber} className='createAccountFormContainer_loginForm_inputTel' onChange={(e) => setPhoneNumber(e.target.value)} />
 
                 <span className='createAccountFormContainer_loginForm_checkboxContainer'>
-                    <input type="checkbox" />
+                    <input type="checkbox" checked={generalConditions} onChange={(e) => setGeneralConditions(e.target.checked)} />
                     <span className='createAccountFormContainer_loginForm_checkboxContainer_checkBoxText'> J'accepte les <Link to="/ConditionsGenerales" className='createAccountFormContainer_loginForm_checkboxContainer_checkBoxText_checkBoxLink'> conditions générales </Link> d'utilisation </span>
                 </span>
-
+                {errorMessage !== '' ? <p className='createAccountFormContainer_loginForm_errorMessage'> {errorMessage} </p> : null}
                 <input type="submit" value="Inscription" className='createAccountFormContainer_loginForm_submitButton' />
 
             </form>
--- src/components/FormCreationTrajet.js
+++ src/components/FormCreationTrajet.js
@@ -5,8 +5,10 @@ import { API_TRAVEL_URL } from '../config';
 const FormCreationTrajet = () => {
 
     const [start, setStart] = useState('');
+    const [departureDate, setDepartureDate] = useState("");
     const [departureTime, setDepartureTime] = useState("");
     const [destination, setDestination] = useState("");
+    const [arrivalDate, setArrivalDate] = useState("");
     const [arrivalTime, setArrivalTime] = useState("");
     const [numberOfPeople, setNumberOfPeople] = useState(1);
     const [flightNumber, setFlightNumber] = useState("");
@@ -36,24 +38,39 @@ const FormCreationTrajet = () => {
             missingFields = missingFields.slice(0, -2); // Supprime la virgule et l'espace à la fin de la chaîne
             setErrorMessage(`Les champs suivants sont manquants: ${missingFields}`);
         } else {
-            if (departureTime >= arrivalTime) {
-                setErrorMessage("L'heure de départ doit être inférieure à l'heure d'arrivée");
-            } else {
-                const newTravel = {
-                    heureDepart: new Date(`1970-01-01T${departureTime}:00.000+01:00`),
-                    heureArrivee: new Date(`1970-01-01T${arrivalTime}:00.000+01:00`),
-                    lieuDepart: start,
-                    lieuArrivee: destination,
-                    nombreDePassagers: numberOfPeople,
-                    numeroDeVol: flightNumber,
-                    idCompte: "A changer plus tard",
-                    idVoyageurs: null
-                }
-                console.log(newTravel);
-                axios.post(`${API_TRAVEL_URL}`, newTravel)
-                    .then((res) => { console.log(res) })
-                    .catch((error) => { console.log(error) });
+
+            const startDate = new Date(departureDate);
+            const startTime = new Date(`1970-01-01T${departureTime}:00.000+01:00`);
+            const startYear = startDate.getFullYear();
+            const startMonth = startDate.getMonth();
+            const startDay = startDate.getDate();
+            const startHours = startTime.getUTCHours();
+            const startMin = startTime.getMinutes();
+            const dateHourStart = new Date(startYear, startMonth, startDay, startHours, startMin);
+
+            const endDate = new Date(arrivalDate);
+            const endTime = new Date(`1970-01-01T${arrivalTime}:00.000+01:00`);
+            const arrivalYear = endDate.getFullYear();
+            const arrivalMonth = endDate.getMonth();
+            const arrivalDay = endDate.getDate();
+            const arrivalHours = endTime.getUTCHours();
+            const arrivalMin = endTime.getMinutes();
+            const dateHourArrival = new Date(arrivalYear, arrivalMonth, arrivalDay, arrivalHours, arrivalMin);
+
+            const newTravel = {
+                heureDepart: dateHourStart,
+                heureArrivee: dateHourArrival,
+                lieuDepart: start,
+                lieuArrivee: destination,
+                nombreDePassagers: numberOfPeople,
+                numeroDeVol: flightNumber,
+                idCompte: "A changer plus tard",
+                idVoyageurs: []
             }
+            console.log(newTravel);
+            axios.post(`${API_TRAVEL_URL}`, newTravel)
+                .then((res) => { console.log(res) })
+                .catch((error) => { console.log(error) });
         }
     }
 
@@ -61,9 +78,19 @@ const FormCreationTrajet = () => {
         <div>
             <form className='form' onSubmit={handleSubmit}>
                 <input className='form_inputsMajeurs' type='text' placeholder="Depart ?" onChange={(e) => setStart(e.target.value)} />
-                <input className='form_inputsMajeurs' type='time' value={departureTime} onChange={(e) => setDepartureTime(e.target.value)} />
+                <input className='form_inputsMajeurs' type='datetime-local'
+                    onChange={(e) => {
+                        const [dateValue, timeValue] = e.target.value.split('T');
+                        setDepartureDate(dateValue);
+                        setDepartureTime(timeValue);
+                    }} />
                 <input className='form_inputsMajeurs' type='text' placeholder="Arrivée ?" onChange={(e) => setDestination(e.target.value)} />
-                <input className='form_inputsMajeurs' type='time' value={arrivalTime} onChange={(e) => setArrivalTime(e.target.value)} />
+                <input className='form_inputsMajeurs' type='datetime-local'
+                    onChange={(e) => {
+                        const [dateValue, timeValue] = e.target.value.split('T');
+                        setArrivalDate(dateValue);
+                        setArrivalTime(timeValue);
+                    }} />
                 <div className='form_conteneurPartieSecondaire'>
                     <div className='form_conteneurPartieSecondaire_conteneurInputs'>
                         <input className='form_conteneurPartieSecondaire_conteneurInputs_inputsSecondaires' type='text' placeholder="N°VOL ?" onChange={(e) => setFlightNumber(e.target.value)} />
@@ -73,7 +100,6 @@ const FormCreationTrajet = () => {
                 </div>
                 <p className='form_texte'> Economise jusqu'à 30€ </p>
                 <input className='form_boutonCreer' type="submit" value="Créer ton trajet gratuitement !" />
-                <p></p>
                 {errorMessage !== '' ? <p className='errorMessage'> {errorMessage} </p> : null}
             </form>
         </div>
